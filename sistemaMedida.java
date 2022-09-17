26f1712e9d0248d96ac7fae55fb879b873098a1d
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-09-16 22:33:12 -0300

    first commit

---
+++ sistemaMedida.java
@@ -0,0 +1,16 @@
+import javax.lang.model.util.ElementScanner14;
+
+public class sistemaMedida {
+    public static void main(String[] args) {
+        String sigla = "M";
+
+        if (sigla == "P")
+            System.out.println("Pequeno");
+        else if (sigla == "M")
+            System.out.println("Medio");
+        else if (sigla == "G")
+            System.out.println("grande");
+        else
+            System.out.println("Indefinido");
+    }
+}
