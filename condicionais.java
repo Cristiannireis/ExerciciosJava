6472a9e6b24d8f15d0401173b9b75eb552cab2f9
Cristiane Reis <43080297+Cristiannireis@users.noreply.github.com>
2022-09-17 22:25:09 -0300

    exercicios condicionais

---
+++ condicionais.java
@@ -0,0 +1,8 @@
+public class condicionais {
+    public static void main(String[] args) {
+        for ( int carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando carneirinhos " + carneirinhos);
+        }
+        System.out.println("Joaozinho dormiu");
+    }
+}
